f772723cd96e6b8ab2e79cb1fedc04e050b4af2b
Pavel <safonaupv@gmail.com>
2019-04-24 15:04:03 +0300

    Initial Commit

---
+++ game2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  game2048
+//
+//  Created by Admin on 24/04/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
